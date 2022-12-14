3a90fa0ff3a979f2f1b64e345c55497fc65abf74
Gabriella Oliveira <45859800+gaabyaolivera@users.noreply.github.com>
2022-12-14 17:04:46 -0300

    simple emoji figther

---
+++ emoji-figther/index.js
@@ -0,0 +1,14 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    
+    let randomNumberOne = Math.floor( Math.random() * fighters.length)
+    let randomNumberTwo = Math.floor( Math.random() * fighters.length)
+    stageEl.textContent = fighters[randomNumberOne] + "VS" + fighters[randomNumberTwo]
+})
